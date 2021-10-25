eae02bd6aa3cf8ad151ca4ec175aad82cb2bd1fe
andreyunugro <andreas@alterra.id>
2021-10-25 10:34:09 +0700

    fix(index.d.ts): fix module declaration

--- index.d.ts
+++ index.d.ts
@@ -1,64 +1,64 @@
+import { ActionHandler } from 'moleculer';
 import TelegramBot from 'node-telegram-bot-api';
 
-declare module 'moleculer-telegram' {
-    export const actions: {
-        sendMessage: {
-            params: {
-                to: string|number;
-				message: string;
-                parse_mode?: TelegramBot.ParseMode;
-				disable_web_page_preview?: boolean;
-				disable_notification?: boolean;
-				reply_to_message_id?: number;
-				reply_markup?: any;
-            };
-            handler: any;
-        };
-
-        sendPhoto: {
-            params: {
-                to: string|number;
-				photo: string;
-                parse_mode?: TelegramBot.ParseMode;
-                caption?: string;
-				disable_notification?: boolean;
-				reply_to_message_id?: number;
-				reply_markup?: any;
-                fileOpts?: {
-                    filename?: string;
-                    contentType?: string;
+declare module 'moleculer-telegram-bot' {
+    type telegramBotService = {
+        name: string;
+        settings: { telegramToken: string },
+        actions: {
+            sendMessage: {
+                params: {
+                    to: string|"number";
+                    message: string;
+                    parse_mode?: TelegramBot.ParseMode;
+                    disable_web_page_preview?: boolean;
+                    disable_notification?: boolean;
+                    reply_to_message_id?: "number";
+                    reply_markup?: any;
                 };
+                handler: ActionHandler;
             };
-            handler: any;
-        };
-
-        sendDocument: {
-            params: {
-                to: string|number;
-				doc: string;
-                parse_mode?: TelegramBot.ParseMode;
-                caption?: string;
-				disable_notification?: boolean;
-				reply_to_message_id?: number;
-				reply_markup?: any;
-                fileOpts?: {
-                    filename?: string;
-                    contentType?: string;
+    
+            sendPhoto: {
+                params: {
+                    to: string|"number";
+                    photo: string;
+                    parse_mode?: TelegramBot.ParseMode;
+                    caption?: string;
+                    disable_notification?: boolean;
+                    reply_to_message_id?: "number";
+                    reply_markup?: any;
+                    fileOpts?: {
+                        filename?: string;
+                        contentType?: string;
+                    };
                 };
+                handler: ActionHandler;
             };
-            handler: any;
-        };
+    
+            sendDocument: {
+                params: {
+                    to: string|"number";
+                    doc: string;
+                    parse_mode?: TelegramBot.ParseMode;
+                    caption?: string;
+                    disable_notification?: boolean;
+                    reply_to_message_id?: "number";
+                    reply_markup?: any;
+                    fileOpts?: {
+                        filename?: string;
+                        contentType?: string;
+                    };
+                };
+                handler: ActionHandler;
+            };
+        },
+        methods: {
+            sendMessage(to: string|number, message: string, opts: TelegramBot.SendMessageOptions): Promise<TelegramBot.Message>;
+            sendPhoto(to: string|number, photo: string, opts: TelegramBot.SendPhotoOptions, fileOpts: telegramBotService["actions"]["sendPhoto"]["params"]["fileOpts"]): Promise<TelegramBot.Message>;
+            sendDocument(to: string|number, doc: string, opts: TelegramBot.SendPhotoOptions, fileOpts: telegramBotService["actions"]["sendPhoto"]["params"]["fileOpts"]): Promise<TelegramBot.Message>;
+        },
     };
 
-    export const methods: {
-        sendMessage(to: string|number, message: string, opts: TelegramBot.SendMessageOptions): Promise<TelegramBot.Message>;
-        sendPhoto(to: string|number, photo: string, opts: TelegramBot.SendPhotoOptions, fileOpts: TelegramBot.fileOpts): Promise<TelegramBot.Message>;
-        sendDocument(to: string|number, doc: string, opts: TelegramBot.SendPhotoOptions, fileOpts: TelegramBot.fileOpts): Promise<TelegramBot.Message>;
-    };
-
-    export const name: string;
-
-    export const settings: {
-        telegramToken: string;
-    };
+    export default function(): telegramBotService;
 }
